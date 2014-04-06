c828fa32cd1e6b7bb4e05cb485df546f96ff9a07
DeoLeung <liangzhanzhao1985@gmail.com>
2014-04-06 12:15:03 +0100

    question 12 code optimization

--- question_12.cpp
+++ question_12.cpp
@@ -7,53 +7,39 @@ using namespace question_12;
 bool findWordFromNode(const vector<vector<char> > &matrix, 
                       const int row, const int column, 
                       set<pair<int, int> > visited, 
-                      const string &word, const int char_index) {
+                      const string &word, const int next_char) {
   // reach the end of the word(we found it)
-  if (char_index == word.size()) return true;
-  const char &target = word[char_index];
+  if (next_char == word.size()) return true;
+  // check if we have visited this node before.
+  const pair<int, int> next = make_pair(row, column);
+  if (visited.find(next) != visited.end()) return false;
+
+  visited.insert(next);
+  const char &target = word[next_char];
+
   // up
-  if (row > 0 && matrix[row - 1][column] == target) {
-    const pair<int, int> next = make_pair(row - 1, column);
-    if (visited.find(next) == visited.end()) {
-      visited.insert(next);
-      if (findWordFromNode(
-          matrix, row - 1, column, visited, word, char_index + 1)) {
+  if (row > 0 && matrix[row - 1][column] == target &&
+      findWordFromNode(
+          matrix, row - 1, column, visited, word, next_char + 1)) {
         return true;
-      }
-    }
   }
   // down
-  if (row < matrix.size() - 1 && matrix[row + 1][column] == target) {
-    const pair<int, int> next = make_pair(row + 1, column);
-    if (visited.find(next) == visited.end()) {
-      visited.insert(next);
-      if (findWordFromNode(
-          matrix, row + 1, column, visited, word, char_index + 1)) {
+  if (row < matrix.size() - 1 && matrix[row + 1][column] == target &&
+      findWordFromNode(
+          matrix, row + 1, column, visited, word, next_char + 1)) {
         return true;
-      }
-    }
   }
   // left
-  if (column > 0 && matrix[row][column - 1] == target) {
-    const pair<int, int> next = make_pair(row, column - 1);
-    if (visited.find(next) == visited.end()) {
-      visited.insert(next);
-      if (findWordFromNode(
-          matrix, row, column - 1, visited, word, char_index + 1)) {
+  if (column > 0 && matrix[row][column - 1] == target &&
+      findWordFromNode(
+          matrix, row, column - 1, visited, word, next_char + 1)) {
         return true;
-      }
-    }
   }
   // right
-  if (column < matrix[row].size() - 1 && matrix[row][column + 1] == target) {
-    const pair<int, int> next = make_pair(row, column + 1);
-    if (visited.find(next) == visited.end()) {
-      visited.insert(next);
-      if (findWordFromNode(
-          matrix, row, column + 1, visited, word, char_index + 1)) {
+  if (column < matrix[row].size() - 1 && matrix[row][column + 1] == target &&
+      findWordFromNode(
+          matrix, row, column + 1, visited, word, next_char + 1)) {
         return true;
-      }
-    }
   }
   return false;
 }
@@ -65,7 +51,6 @@ bool question_12::findWordInMatrix(
     for (int c = 0; c < matrix[r].size(); ++c) {
       if (matrix[r][c] == word[0]) {
         set<pair<int, int> > visited;
-        visited.insert(make_pair(r, c));
         if (findWordFromNode(matrix, r, c, visited, word, 1)) return true;
       }
     }
